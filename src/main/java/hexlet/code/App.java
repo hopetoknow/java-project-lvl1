24371f26897d0e876e692ae0fcaf1335310969ce
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-22 21:03:27 +0300

    add greet game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,18 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetUser();
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit
+                Your choice:\s""");
+        Scanner sc = new Scanner(System.in);
+        int gameNumber = sc.nextInt();
+        if (gameNumber == 1) {
+            Cli.greetUser();
+        }
     }
 }
